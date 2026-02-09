be2c59fabc680886c250cb315a6b56ec3de55778
Jesús Fonseca Serrano <jesusfonseruma@gmail.com>
2026-02-09 21:36:17 +0100

    Paso 8: Usando modelo JSON

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,24 @@
 
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], function (Controller, MessageToast) {
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
     "use strict"
     return Controller.extend("sap.ui.demo.walkthrough.App",{
         onShowHello : function () {
             MessageToast.show("Hola cómo estás");
+        },
+
+        onInit: function(){
+            const oData = {
+                recipient: {
+                    name: "UI5"
+                }
+            };
+
+            let oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
         }
     });
 });
