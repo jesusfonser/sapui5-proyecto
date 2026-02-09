d04b832b5a7457cb0d0f5a339eb0701554c50f60
Jesús Fonseca Serrano <jesusfonseruma@gmail.com>
2026-02-09 21:18:50 +0100

    Paso 7: Creando mensaje Toast

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,12 @@
 
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], function (Controller, MessageToast) {
     "use strict"
     return Controller.extend("sap.ui.demo.walkthrough.App",{
         onShowHello : function () {
-            alert("Hola que taaal");
+            MessageToast.show("Hola cómo estás");
         }
     });
 });
