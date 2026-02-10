c90721d599eb8265e321d6b79525461f49891ec0
Jesús Fonseca Serrano <jesusfonseruma@gmail.com>
2026-02-10 15:36:16 +0100

    Paso 9: utilizando textos traducibles

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -2,12 +2,17 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict"
     return Controller.extend("sap.ui.demo.walkthrough.App",{
         onShowHello : function () {
-            MessageToast.show("Hola cómo estás");
+            const oBundle = this.getView().getModel("i18n").getResourceBundle();
+            const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            const sMsg = oBundle.getText("helloMsg", [sRecipient])
+
+            MessageToast.show(sMsg);
         },
 
         onInit: function(){
@@ -19,6 +24,13 @@ sap.ui.define([
 
             let oModel = new JSONModel(oData);
             this.getView().setModel(oModel);
+
+            const i18nModel = new ResourceModel({
+                bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
+                supportedLocales: [""],
+                fallbackLocale: ""
+            })
+            this.getView().setModel(i18nModel, "i18n");
         }
     });
 });
